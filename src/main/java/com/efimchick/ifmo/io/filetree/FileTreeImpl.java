39d1f3f62cb92255a528a3f902762ba7b0f5b2b6
borrowsorrow <51077910+borrowsorrow@users.noreply.github.com>
2022-04-02 02:09:11 +0300

    completing task FileTreeImpl

--- src/main/java/com/efimchick/ifmo/io/filetree/FileTreeImpl.java
+++ src/main/java/com/efimchick/ifmo/io/filetree/FileTreeImpl.java
@@ -1,5 +1,115 @@
 package com.efimchick.ifmo.io.filetree;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
 public class FileTreeImpl implements FileTree {
 
+    private static final String NEW_DIRECTORY_SYMBOL = "├─ ";
+    private static final String NEW_FILE_IN_DIRECTORY_SYMBOL = "│  ";
+    private static final String LAST_FILE_IN_DIRECTORY = "└─ ";
+    private static final String BLANK_SPACE = "   ";
+    private static final String WORD_DELIMITIER = " ";
+    private static final String SIZE_MEASURE = " bytes";
+
+    @Override
+    public Optional<String> tree(Path path) {
+        File file = new File(String.valueOf(path));
+        Optional<String> result = Optional.empty();
+        if (file.isFile()) {
+            result = Optional.of(getFileData(file));
+        }
+        if (file.isDirectory()) {
+            result = Optional.of(printFileTree(file, new ArrayList<>()));
+        }
+        return result;
+    }
+
+    private String printFileTree(File currentFolder, List<Boolean> isParentFolderLastInDirectoryList) {
+        StringBuilder directory = new StringBuilder();
+        if (!isParentFolderLastInDirectoryList.isEmpty()) {
+            directory.append(addNotRootFilePathInformation(isParentFolderLastInDirectoryList.
+                    get(isParentFolderLastInDirectoryList.size() - 1)));
+        }
+        directory.append(getFolderSizeData(currentFolder));
+        List<File> files = sortFiles(currentFolder.listFiles());
+        int count = files.size();
+        for (int i = 0; i < count; i++) {
+            boolean isFileLastInDirectory = count == i + 1;
+            directory.append(addChildFileInformation(files.get(i),
+                    isFileLastInDirectory, isParentFolderLastInDirectoryList));
+        }
+        return directory.toString();
+    }
+
+    private String addNotRootFilePathInformation(boolean isParentFolderLastInDirectoryList) {
+        return isParentFolderLastInDirectoryList ? LAST_FILE_IN_DIRECTORY : NEW_DIRECTORY_SYMBOL;
+    }
+
+    private String addChildFileInformation (File file, boolean isFileLastInDirectory,
+                                            List<Boolean> isParentFolderLastInDirectoryList) {
+        StringBuilder directory = new StringBuilder();
+        directory.append(addFilePathInformation(isParentFolderLastInDirectoryList));
+        if (file.isFile()) {
+            directory.append(isFileLastInDirectory ? LAST_FILE_IN_DIRECTORY : NEW_DIRECTORY_SYMBOL);
+            directory.append(getFileData(file));
+        } else {
+            ArrayList<Boolean> childIsParentFolderLastInDirectoryList =
+                    new ArrayList<>(isParentFolderLastInDirectoryList);
+            childIsParentFolderLastInDirectoryList.add(isFileLastInDirectory);
+            directory.append(printFileTree(file, childIsParentFolderLastInDirectoryList));
+        }
+        return directory.toString();
+    }
+
+    private long getFolderSize(File folder) {
+        long folderSize = 0;
+        File[] files = folder.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    folderSize += file.length();
+                } else {
+                    folderSize += getFolderSize(file);
+                }
+            }
+        }
+        return folderSize;
+    }
+
+    private String getFolderSizeData(File currentFolder) {
+        return currentFolder.getName() + WORD_DELIMITIER + getFolderSize(currentFolder) + SIZE_MEASURE;
+    }
+
+    private String getFileData(File file) {
+        return file.getName() + WORD_DELIMITIER + file.length() + SIZE_MEASURE;
+    }
+
+    private String addFilePathInformation(List<Boolean> isParentFolderLastInDirectoryList) {
+        StringBuilder directoryPath = new StringBuilder("\n");
+        for (boolean isParentFolderLast : isParentFolderLastInDirectoryList) {
+            if (isParentFolderLast) {
+                directoryPath.append(BLANK_SPACE);
+            } else {
+                directoryPath.append(NEW_FILE_IN_DIRECTORY_SYMBOL);
+            }
+        }
+        return directoryPath.toString();
+    }
+
+    private List<File> sortFiles(File[] folder) {
+        Arrays.sort(folder);
+        Set<File> sorted = new TreeSet<>(Comparator.
+                comparing((File file) -> !file.isDirectory()).thenComparing(File::compareTo));
+        sorted.addAll(Arrays.stream(folder).collect(Collectors.toList()));
+        return new ArrayList<>(sorted);
+    }
 }
